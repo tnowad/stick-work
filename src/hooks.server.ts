b9642066140dcc761492e55b441bba2390fc7e5f
Nguyen Minh Tuan <tnowad@gmail.com>
2024-04-23 00:43:40 +0700

    feat: add check access before request

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,20 @@
 import admin from '$lib/firebase/firebase.admin';
-import type { Handle, RequestEvent } from '@sveltejs/kit';
+import { error, redirect, type Handle, type RequestEvent } from '@sveltejs/kit';
 import type { DecodedIdToken } from 'firebase-admin/auth';
 
-const publicPaths = new Set(['/', '/sign-in', '/sign-up']);
 const authRequiredPaths = new Set(['/profile']);
 const adminRequiredPaths = new Set(['/admin']);
+
+const isAuthRequiredPath = (path: string) => authRequiredPaths.has(path);
+const isAdminRequiredPath = (path: string) => adminRequiredPaths.has(path);
+
+const isAuth = (event: RequestEvent) => event?.locals?.user;
+const isAdmin = (event: RequestEvent) => event?.locals?.user?.role === 'admin';
 
 export const handle: Handle = async ({ event, resolve }) => {
   await tryGetCurrentUser(event);
   await tryGetRole(event);
+  await checkAccess(event);
 
   const response = await resolve(event);
   return response;
@@ -36,5 +42,15 @@ const tryGetRole = async (event: RequestEvent) => {
     event.locals.user.role = user.customClaims?.role || 'user';
   } catch (error) {
     console.error(error);
+  }
+};
+
+const checkAccess = async (event: RequestEvent) => {
+  if (isAuthRequiredPath(event.url.pathname) && !isAuth(event)) {
+    throw redirect(302, '/sign-in');
+  }
+
+  if (isAdminRequiredPath(event.url.pathname) && !isAdmin(event)) {
+    throw error(403, 'Forbidden');
   }
 };
