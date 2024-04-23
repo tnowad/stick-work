b40ea2c083678e7ef3a9dc524617972facc9b7f8
Nguyen Minh Tuan <tnowad@gmail.com>
2024-04-23 22:51:54 +0700

    feat: add sign-up page

---
+++ src/routes/sign-up/+page.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import { enhance } from '$app/forms';
+  import { firebaseAuth } from '$lib/firebase/firebase.app';
+  import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+</script>
+
+<section class="hero min-h-screen bg-base-200">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <div class="text-center lg:text-left">
+      <h1 class="text-5xl font-bold">Sign Up Now!</h1>
+      <p class="py-6">Create an account to start using our services.</p>
+    </div>
+    <div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form class="card-body pb-0">
+        <div class="form-control">
+          <label class="label" for="emailInput">
+            <span class="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="email"
+            id="emailInput"
+            class="input input-bordered"
+            required
+          />
+        </div>
+        <div class="form-control">
+          <label class="label" for="passwordInput">
+            <span class="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="password"
+            id="passwordInput"
+            class="input input-bordered"
+            required
+          />
+        </div>
+        <div class="form-control">
+          <button class="btn btn-primary">Sign Up</button>
+        </div>
+        <div class="label">
+          <span class="label-text-alt"
+            >Already have an account? <a href="/sign-in" class="label-text-alt link link-hover"
+              >Sign in now</a
+            >
+          </span>
+        </div>
+      </form>
+      <form
+        class="card-body pt-0"
+        method="post"
+        action="?/signUpWithGoogle"
+        use:enhance={async ({ formData }) => {
+          const credential = await signInWithPopup(firebaseAuth, new GoogleAuthProvider());
+          const idToken = await credential.user.getIdToken();
+          formData.set('idToken', idToken);
+
+          return ({ result }) => {
+            console.log(result);
+          };
+        }}
+      >
+        <div class="divider">OR</div>
+        <div class="form-control">
+          <button class="btn">Sign Up with Google</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</section>
