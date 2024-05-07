c85af654a96501228b516fe319f978a7fef284e0
Nguyen Minh Tuan <tnowad@gmail.com>
2024-05-08 01:34:14 +0700

    chore: add note features in shedule page

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -27,4 +27,16 @@
 </script>
 
 <div class="w-full min-h-screen">
+  <!-- Header show current date, button to change view (day, week, month, agenda) -->
+  <!-- Sidebar show calender, filter events -->
+
+  <!--
+    Show week view contain all events in week
+    headers: day of week (Mon, Tue, Wed, Thu, Fri, Sat, Sun) and date
+    show global events and user events from all calendars
+    show events in day view (show all events)
+    show events in week view (show all events)
+    show events in month view (show only event all day)
+    show agenda view (show all events)
+  -->
 </div>
