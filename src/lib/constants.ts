20398ecd86f54e1b52186f25ecc922a4a009d412
Nguyen Minh Tuan <tnowad@gmail.com>
2024-05-20 01:57:03 +0700

    refactor: move sign-in, sign-up, sign-out to auth dir

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,9 +2,9 @@ export enum AppRoute {
   HOME = '/',
   PROFILE = '/profile',
 
-  AUTH_SIGN_IN = '/sign-in',
-  AUTH_SIGN_UP = '/sign-up',
-  AUTH_SIGN_OUT = '/sign-out',
+  AUTH_SIGN_IN = '/auth/sign-in',
+  AUTH_SIGN_UP = '/auth/sign-up',
+  AUTH_SIGN_OUT = '/auth/sign-out',
 
   DASHBOARD = '/dashboard',
   DASHBOARD_USER_MANAGEMENT = '/dashboard/users'
