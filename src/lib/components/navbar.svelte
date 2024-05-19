5848a0c71d29a213381f91f3c90efa93374e4cdb
Nguyen Minh Tuan <tnowad@gmail.com>
2024-05-20 02:00:24 +0700

    refactor: change magic href to enum

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import ThemeSelect from '$lib/components/theme-select.svelte';
+  import { AppRoute } from '$lib/constants';
   import type { DecodedIdToken } from 'firebase-admin/auth';
 
   export let user: DecodedIdToken | undefined;
@@ -8,7 +9,7 @@
 
 <div class="navbar bg-base-100">
   <div class="flex-1">
-    <a href="/" class="btn btn-ghost text-xl">Stick Work</a>
+    <a href={AppRoute.HOME} class="btn btn-ghost text-xl">Stick Work</a>
   </div>
   <div class="flex-none gap-2">
     <div class="form-control">
@@ -28,22 +29,22 @@
           class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
         >
           <li>
-            <a href="/profile" class="justify-between">
+            <a href={AppRoute.PROFILE} class="justify-between">
               Profile
               <span class="badge">New</span>
             </a>
           </li>
-          <li><a href="/settings">Settings</a></li>
+          <li><a href={AppRoute.SETTINGS}>Settings</a></li>
 
           {#if role === 'admin'}
-            <li><a href="/dashboard">Dashboard</a></li>
+            <li><a href={AppRoute.DASHBOARD}>Dashboard</a></li>
           {/if}
 
-          <li><a href="/sign-out">Sign out</a></li>
+          <li><a href={AppRoute.AUTH_SIGN_OUT}>Sign out</a></li>
         </ul>
       </div>
     {:else}
-      <a href="/sign-in" class="btn btn-primary">Sign In</a>
+      <a href={AppRoute.AUTH_SIGN_IN} class="btn btn-primary">Sign In</a>
     {/if}
   </div>
 </div>
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,7 @@
 export enum AppRoute {
   HOME = '/',
   PROFILE = '/profile',
+  SETTINGS = '/settings',
 
   AUTH_SIGN_IN = '/auth/sign-in',
   AUTH_SIGN_UP = '/auth/sign-up',
