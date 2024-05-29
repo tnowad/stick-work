019a97841f4c1bdf479f6e1534c99782ba93c1a0
Nguyen Minh Tuan <tnowad@gmail.com>
2024-05-30 04:43:28 +0700

    feat: change loading bar to gif loading and preload image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,10 +18,13 @@
 
 <svelte:head>
   <title>{data.meta.title}</title>
+  <link rel="preload" href="/loading.gif" as="image" />
 </svelte:head>
 
 {#if isNavigating}
-  <span class="loading loading-ring loading-lg fixed bottom-0 right-0 m-4"></span>
+  <span class="fixed bottom-0 right-0 m-4">
+    <img src="/loading.gif" class="w-20 h-20" alt="loading" />
+  </span>
 {/if}
 
 <Navbar user={data.user} role={data.role} />
