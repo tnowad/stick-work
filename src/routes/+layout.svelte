d7d33dfff4fd327687e74c518baf4c5689a53b17
Nguyen Minh Tuan <tnowad@gmail.com>
2024-05-30 04:30:13 +0700

    feat: add loading when navigate

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
+  import { navigating } from '$app/stores';
   import Navbar from '$lib/components/navbar.svelte';
   import type { LayoutData } from './$types';
   import './global.css';
   export let data: LayoutData;
+
+  let isNavigating = false;
+
+  navigating.subscribe((value) => {
+    if (value) {
+      isNavigating = true;
+    } else {
+      isNavigating = false;
+    }
+  });
 </script>
 
 <svelte:head>
   <title>{data.meta.title}</title>
 </svelte:head>
+
+{#if isNavigating}
+  <span class="loading loading-ring loading-lg fixed bottom-0 right-0 m-4"></span>
+{/if}
 
 <Navbar user={data.user} role={data.role} />
 
