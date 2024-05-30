d5a347ba006d3a7197f5e1ac18dd51ae9fca2a09
Nguyen Minh Tuan <tnowad@gmail.com>
2024-05-30 15:47:47 +0700

    feat: add load profile data

---
+++ src/routes/profile/+page.server.ts
@@ -0,0 +1,28 @@
+import admin from '$lib/firebase/firebase.admin';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ locals }) => {
+  if (!locals.user) {
+    return;
+  }
+  const user = await admin.auth().getUser(locals.user.uid);
+
+  const { uid, email, displayName, photoURL, disabled, phoneNumber, emailVerified, customClaims } =
+    user;
+
+  return {
+    user: {
+      uid,
+      email,
+      displayName,
+      photoURL,
+      disabled,
+      phoneNumber,
+      emailVerified,
+      customClaims
+    },
+    meta: {
+      title: 'Profile'
+    }
+  };
+};
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,1 +1,8 @@
-profile
+<script lang="ts">
+  export let data;
+</script>
+
+<main>
+  <h1>User</h1>
+  <pre>{JSON.stringify(data, null, 2)}</pre>
+</main>
