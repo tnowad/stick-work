05bba516b330c38778db12c34de810d28251c6aa
Nguyen Minh Tuan <tnowad@gmail.com>
2024-06-03 18:44:31 +0700

    feat: add field for type event

--- src/lib/types/index.d.ts
+++ src/lib/types/index.d.ts
@@ -1,23 +1,86 @@
 export type Event = {
   id: string;
   name: string;
-  start: Date;
-  end: Date;
-  repeat?: {
-    type: 'daily' | 'weekly' | 'monthly' | 'yearly';
-    interval: number;
-    start: Date;
-    end: Date;
-  };
-  allDay?: {
-    start: Date;
-    end: Date;
-  };
-  description?: string;
+
+  description: string;
+
+  startTime: Date;
+
+  endTime: Date;
+
+  isAllDay: boolean;
+
+  recurrence: Recurrence;
+
   calendarId: string;
+
+  calendar?: Calendar;
+
+  userId: string;
 };
 
 export type Calendar = {
   id: string;
+
   name: string;
+
+  events?: Event[];
+
+  userId: string;
+};
+
+type Recurrence =
+  | RecurrenceNone
+  | RecurrenceDaily
+  | RecurrenceWeekly
+  | RecurrenceMonthly
+  | RecurrenceYearly
+  | RecurrenceCustom;
+
+type RecurrenceNone = {
+  type: 'none';
+};
+
+type RecurrenceDaily = {
+  type: 'daily';
+
+  interval: number;
+
+  endDate?: Date;
+};
+
+type RecurrenceWeekly = {
+  type: 'weekly';
+
+  interval: number;
+
+  endDate?: Date;
+
+  daysOfWeek: number[];
+};
+
+type RecurrenceMonthly = {
+  type: 'monthly';
+
+  interval: number;
+
+  endDate?: Date;
+
+  daysOfMonth: number[];
+};
+
+type RecurrenceYearly = {
+  type: 'yearly';
+
+  interval: number;
+
+  endDate?: Date;
+
+  monthsOfYear: number[];
+};
+
+type RecurrenceCustom = {
+  type: 'custom';
+
+  customDates: Date[];
 };
