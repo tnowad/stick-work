1eea91f234d95b8a0170eb90e0663c60aa5efc80
Nguyen Minh Tuan <tnowad@gmail.com>
2024-06-03 17:46:34 +0700

    feat: add form create event

--- src/routes/(user)/schedule/+page.svelte
+++ src/routes/(user)/schedule/+page.svelte
@@ -32,7 +32,7 @@
     <button type="submit" class="p-2 ml-2 bg-blue-500 text-white rounded-md"> Search </button>
   </form>
 
-  <form method="post" action="?/createCalendar" use:enhance>
+  <form method="post" action="?/createCalendar" use:enhance class="card max-w-sm card-bordered">
     <h3>Create Calendar</h3>
     <label for="calendar-name">Name</label>
     <input type="text" name="calendarName" id="calendar-name" />
@@ -41,7 +41,48 @@
       <p>{form.errors?.calendarName}</p>
     {/if}
 
-    <button type="submit">Create</button>
+    <button type="submit" class="btn btn-primary btn-sm">Create</button>
+  </form>
+
+  <form method="post" action="?/createEvent" use:enhance class="card max-w-sm card-bordered">
+    <h3>Create Event</h3>
+
+    <label for="event-calendar">Calendar</label>
+    <select name="eventCalendar" id="event-calendar">
+      {#each data.calendars as calendar}
+        <option value={calendar.id}>{calendar.name}</option>
+      {/each}
+    </select>
+
+    <label for="event-name">Name</label>
+    <input type="text" name="eventName" id="event-name" />
+
+    {#if form?.errors?.eventName}
+      <p>{form.errors?.eventName}</p>
+    {/if}
+
+    <label for="event-start">Start</label>
+    <input type="datetime-local" name="eventStart" id="event-start" />
+
+    {#if form?.errors?.eventStart}
+      <p>{form.errors?.eventStart}</p>
+    {/if}
+
+    <label for="event-end">End</label>
+    <input type="datetime-local" name="eventEnd" id="event-end" />
+
+    {#if form?.errors?.eventEnd}
+      <p>{form.errors?.eventEnd}</p>
+    {/if}
+
+    <label for="event-description">Description</label>
+    <textarea name="eventDescription" id="event-description"></textarea>
+
+    {#if form?.errors?.eventDescription}
+      <p>{form.errors?.eventDescription}</p>
+    {/if}
+
+    <button type="submit" class="btn btn-primary btn-sm">Create</button>
   </form>
 
   <form>
@@ -51,6 +92,18 @@
         <li>
           <input type="checkbox" id={calendar.id} name="calendars" value={calendar.id} />
           <label for={calendar.id}>{calendar.name}</label>
+        </li>
+      {/each}
+    </ul>
+  </form>
+
+  <form>
+    <h3>Events</h3>
+    <ul>
+      {#each data.events as event}
+        <li>
+          <input type="checkbox" id={event.id} name="events" value={event.id} />
+          <label for={event.id}>{event.name}</label>
         </li>
       {/each}
     </ul>
