483da0b9ada75862ce5ea715e33b55a4a62b64cd
Nguyen Minh Tuan <tnowad@gmail.com>
2024-06-04 19:14:38 +0700

    feat: add check user and change title

--- src/routes/dashboard/users/new/+page.server.ts
+++ src/routes/dashboard/users/new/+page.server.ts
@@ -1,6 +1,13 @@
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({}) => {
+export const load: PageServerLoad = async ({ locals }) => {
+  if (!locals.user) {
+    return {
+      status: 401,
+      error: new Error('Unauthorized')
+    };
+  }
+
   // load some things to create user like roles, permissions, etc.
   return {
     meta: {
--- src/routes/dashboard/users/new/+page.svelte
+++ src/routes/dashboard/users/new/+page.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+  const { data } = $props();
 </script>
+
+<svelte:head>
+  <title>{data.meta.title}</title>
+</svelte:head>
 
 Create user from
