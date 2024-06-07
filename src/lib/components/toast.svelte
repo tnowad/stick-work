e29a8dac54e817c5a8b90fcf3883fe26d0e73f42
Nguyen Minh Tuan <tnowad@gmail.com>
2024-06-07 14:45:50 +0700

    feat: add show toast

--- src/lib/components/toast.svelte
+++ src/lib/components/toast.svelte
@@ -1,6 +1,27 @@
 <script lang="ts">
   import { toaster, type Toast } from '$lib/stores/toasts.store';
+  import { cn } from '$lib/utils/classnames';
+  import Icon from '@iconify/svelte';
   import { onDestroy } from 'svelte';
+
+  const TOAST_TYPES = {
+    info: {
+      class: 'alert-info',
+      icon: 'heroicons:information-circle'
+    },
+    success: {
+      class: 'alert-success',
+      icon: 'heroicons:check-circle'
+    },
+    warning: {
+      class: 'alert-warning',
+      icon: 'heroicons:exclamation-circle'
+    },
+    error: {
+      class: 'alert-error',
+      icon: 'heroicons:x-circle'
+    }
+  };
 
   let history: Toast[] = $state([]);
 
@@ -25,3 +46,37 @@
   });
 </script>
 
+<div class="toast">
+  {#each $toaster as toast (toast.id)}
+    <div
+      class={cn(
+        'max-w-sm alert whitespace-normal text-white',
+        toast.type && TOAST_TYPES[toast.type].class
+      )}
+    >
+      {#if toast.type}
+        <Icon icon={TOAST_TYPES[toast.type].icon} class="w-6 h-6" />
+      {/if}
+      <div>
+        {#if toast.title}
+          <h3 class="font-bold">{toast.title}</h3>
+        {/if}
+
+        <span class={toast.title ? 'text-sm' : ''}>
+          {#each (toast.message ?? '').split('\n') as line}
+            {line}<br />
+          {/each}
+        </span>
+      </div>
+      {#if toast.closable}
+        <button
+          class="btn btn-sm btn-circle btn-ghost"
+          onclick={() => toaster.remove(toast.id)}
+          onkeydown={() => toaster.remove(toast.id)}
+        >
+          <Icon icon="heroicons:x-mark" class="w-5 h-5" />
+        </button>
+      {/if}
+    </div>
+  {/each}
+</div>
