cc39965a5be769157d81cbdc5e0760687a156d7e
Nguyen Minh Tuan <tnowad@gmail.com>
2024-06-10 02:13:11 +0700

    feat: add define ability for user

--- src/lib/abilities/define.ability.ts
+++ src/lib/abilities/define.ability.ts
@@ -1,3 +1,7 @@
+import type { User } from '$lib/types';
+import { Role } from '$lib/types/user.type';
+import { defineAbility } from '@casl/ability';
+
 enum Actions {
   MANAGE = 'manage',
   CREATE = 'create',
@@ -14,3 +18,26 @@ enum Subjects {
   ROLE = 'role'
 }
 
+export const defineAbilitiesForUser = (user?: User) => {
+  return defineAbility((can) => {
+    if (!user) {
+      return;
+    }
+
+    switch (user.role) {
+      case Role.ADMIN:
+        can(Actions.MANAGE, Subjects.DASHBOARD);
+        can(Actions.MANAGE, Subjects.USER);
+        can(Actions.MANAGE, Subjects.ROLE);
+        can(Actions.MANAGE, Subjects.CALENDAR);
+        can(Actions.MANAGE, Subjects.EVENT);
+        break;
+      case Role.USER:
+        can([Actions.READ, Actions.UPDATE, Actions.DELETE], [Subjects.CALENDAR, Subjects.EVENT], {
+          userId: user.uid
+        });
+        break;
+    }
+    can([Actions.READ, Actions.UPDATE], Subjects.USER, { uid: user.uid });
+  });
+};
