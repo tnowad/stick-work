367c1c3c35da9d308216083675b46b010d2bea5b
Nguyen Minh Tuan <tnowad@gmail.com>
2024-06-12 05:04:18 +0700

    feat: add routes configs

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,8 @@
+import { redirect } from '@sveltejs/kit';
+import { Action, Subject } from './abilities/define.ability';
+import type { AppRoutesConfigs } from './types';
+import { StatusCodes } from 'http-status-codes';
+
 export enum AppRoute {
   HOME = '/',
   PROFILE = '/profile',
@@ -10,10 +15,82 @@ export enum AppRoute {
   AUTH_FORGOT_PASSWORD = '/auth/forgot-password',
 
   DASHBOARD = '/dashboard',
-  DASHBOARD_USER_MANAGEMENT = '/dashboard/users'
+
+  DASHBOARD_USER_MANAGEMENT = '/dashboard/users',
+  DASHBOARD_USER_DETAIL = '/dashboard/users/[id]',
+  DASHBOARD_USER_EDIT = '/dashboard/users/[id]/edit',
+  DASHBOARD_USER_CREATE = '/dashboard/users/create',
+  DASHBOARD_USER_DELETE = '/dashboard/users/[id]/delete'
 }
 
 export enum Theme {
   LIGHT = 'light',
   DARK = 'dark'
 }
+
+export const appRoutesConfigs: AppRoutesConfigs = {
+  // Public routes
+  [AppRoute.HOME]: {
+    subject: Subject.PUBLIC,
+    action: Action.READ
+  },
+  [AppRoute.AUTH_SIGN_IN]: {
+    subject: Subject.PUBLIC,
+    action: Action.READ
+  },
+  [AppRoute.AUTH_SIGN_UP]: {
+    subject: Subject.PUBLIC,
+    action: Action.READ
+  },
+  [AppRoute.AUTH_FORGOT_PASSWORD]: {
+    subject: Subject.PUBLIC,
+    action: Action.READ
+  },
+
+  // Authenticated routes
+  [AppRoute.PROFILE]: {
+    subject: Subject.AUTHENTICATED,
+    action: Action.READ,
+    onReject: () => redirect(StatusCodes.TEMPORARY_REDIRECT, AppRoute.AUTH_SIGN_IN)
+  },
+  [AppRoute.SETTINGS]: {
+    subject: Subject.AUTHENTICATED,
+    action: Action.READ,
+    onReject: () => redirect(StatusCodes.TEMPORARY_REDIRECT, AppRoute.AUTH_SIGN_IN)
+  },
+  [AppRoute.SCHEDULE]: {
+    subject: Subject.AUTHENTICATED,
+    action: Action.READ,
+    onReject: () => redirect(StatusCodes.TEMPORARY_REDIRECT, AppRoute.AUTH_SIGN_IN)
+  },
+  [AppRoute.AUTH_SIGN_OUT]: {
+    subject: Subject.AUTHENTICATED,
+    action: Action.READ
+  },
+
+  // Admin routes
+  [AppRoute.DASHBOARD]: {
+    subject: Subject.DASHBOARD,
+    action: Action.READ
+  },
+  [AppRoute.DASHBOARD_USER_MANAGEMENT]: {
+    subject: Subject.USER,
+    action: Action.MANAGE
+  },
+  [AppRoute.DASHBOARD_USER_DETAIL]: {
+    subject: Subject.USER,
+    action: Action.MANAGE
+  },
+  [AppRoute.DASHBOARD_USER_EDIT]: {
+    subject: Subject.USER,
+    action: Action.MANAGE
+  },
+  [AppRoute.DASHBOARD_USER_CREATE]: {
+    subject: Subject.USER,
+    action: Action.MANAGE
+  },
+  [AppRoute.DASHBOARD_USER_DELETE]: {
+    subject: Subject.USER,
+    action: Action.MANAGE
+  }
+};
